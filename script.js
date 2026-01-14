5dc7de0ddd24f35d34f3589a9c98956146739127
SirioRedGiant <Oiris1992@gmail.com>
2026-01-14 01:25:39 +0100

    logica funzionante e correzzioni di nomenclature

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //note Seleziono il bottone dall'HTML
 const btnCalculate = document.getElementById("calculate");
+const btnReset = document.getElementById("reset");
 
 //note Resto in ascolto del click sul bottone
 btnCalculate.addEventListener("click", function () {
-  //note     1. Raccolta dati dagli input
+  //**    1. Raccolta dati dagli input
   const name = document.getElementById("user-name").value;
   const km = parseFloat(document.getElementById("km").value);
-  const age = parseInt(document.getElementById("age").value);
+  const ageBracket = document.getElementById("age-bracket").value;
   const pricePerKm = 0.21;
 
   //*     2. Calcolo prezzo pieno del biglietto
@@ -14,29 +15,34 @@ btnCalculate.addEventListener("click", function () {
   let ticketPriceOffer = "biglietto standard";
 
   //^     3. Applicazione sconti in base all'età
-  if (age - bracket === "minor") {
+  if (ageBracket === "minor") {
     // Sconto 20% per minorenni
     totalPrice = totalPrice - totalPrice * 0.2;
     ticketPriceOffer = "Minor-ticket (20%)";
-  } else if (age - bracket > "senior") {
+  } else if (ageBracket === "senior") {
     // Sconto 40% per over 65
     totalPrice = totalPrice - totalPrice * 0.4;
     ticketPriceOffer = "Over 65 (40%)";
   }
   // Sconto non applicate agli adulti
 
-  //!     4. Output finale in console con 2 decimali
-  console.log("Chilometri impostati:", km);
-  console.log("Età impostata:", age);
-  console.log("Prezzo finale: €", totalPrice.toFixed(2));
+  //!     Stampa in pagina e mostra il biglietto
+  document.getElementById("ticket-title").style.display = "block";
+  document.getElementById("ticket-area").style.display = "block";
+
+  // Assegnazione valori ai campi del biglietto
   document.getElementById("display-name").innerText = " " + name;
-  document.getElementById("display-kilometers").innerText = " " + km + " km";
   document.getElementById("display-provided-offer").innerText =
     " " + ticketPriceOffer;
   document.getElementById("display-ticket-price").innerText =
     " " + totalPrice.toFixed(2) + " €";
+});
 
-  console.log(
-    "Prezzo finale calcolato per " + name + ": €" + totalPrice.toFixed(2)
-  );
+//note Funzione  per il pulsante Annulla
+btnReset.addEventListener("click", function () {
+  document.getElementById("user-name").value = "";
+  document.getElementById("km").value = "";
+  document.getElementById("age-bracket").value = "adult";
+  document.getElementById("ticket-title").style.display = "none";
+  document.getElementById("ticket-area").style.display = "none";
 });
